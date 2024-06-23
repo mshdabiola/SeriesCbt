b4b27e27364ec68fdd78075dd26c6181616fcdb7
mshdabiola <mshdabiola@gmail.com>
2024-06-23 11:41:28 +0100

    feat: initial commit
    This commit introduces the initial structure of the CBT app, including:
    
    - A new MainActivity for Android.
    - Basic UI setup with
     CbtApp.
    - Dependency injection using Koin.
    - Integration with Firebase for Google Play flavor.
    - Configuration for different build flavors (fossReliant and googlePlay).
    - Proguard rules and build configurations.

--- cbtApp/build.gradle.kts
+++ cbtApp/build.gradle.kts
@@ -8,11 +8,10 @@ plugins {
     id("mshdabiola.android.application")
     id("mshdabiola.android.application.compose")
     id("mshdabiola.android.application.jacoco")
-    id("mshdabiola.android.application.firebase")
-    alias(libs.plugins.androidx.baselineprofile)
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.roborazzi)
+    id("mshdabiola.android.application.flavor")
     alias(libs.plugins.conveyor)
+    alias(libs.plugins.baselineprofile)
+    alias(libs.plugins.roborazzi)
 
 }
 
@@ -37,29 +36,60 @@ dependencies {
 
     implementation(libs.koin.android)
 
-    implementation(libs.androidx.metrics)
 
-    debugImplementation(libs.leakcanary.android)
 
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material3.adaptive)
+    implementation(libs.androidx.compose.material3.adaptive.layout)
+    implementation(libs.androidx.compose.material3.adaptive.navigation)
+//    implementation(libs.androidx.compose.material3.windowSizeClass)
+    implementation(libs.androidx.compose.runtime.tracing)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.core.splashscreen)
-    implementation(libs.androidx.tracing.ktx)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.profileinstaller)
+    implementation(libs.androidx.tracing.ktx)
+    implementation(libs.androidx.window.core)
+    implementation(libs.kotlinx.coroutines.guava)
+    implementation(libs.coil.kt)
+
 
     debugImplementation(libs.androidx.compose.ui.testManifest)
 
 
-    testImplementation(project(":modules:testing"))
-    testImplementation(libs.accompanist.testharness)
+    testImplementation(projects.modules.testing)
+    testImplementation(libs.androidx.compose.ui.test)
+    testImplementation(libs.androidx.work.testing)
 
-    testImplementation(libs.robolectric)
-    testImplementation(libs.roborazzi)
+    testFossReliantImplementation(libs.robolectric)
+    testFossReliantImplementation(libs.roborazzi)
+    //testFossReliantImplementation(projects.modules.screenshotTesting)
 
-    androidTestImplementation(project(":modules:testing"))
-    androidTestImplementation(libs.accompanist.testharness)
-    debugImplementation(libs.androidx.monitor)
-    baselineProfile(project(":benchmarks"))
+
+    androidTestImplementation(projects.modules.testing)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(libs.androidx.navigation.testing)
+    androidTestImplementation(libs.androidx.compose.ui.test)
+
+    baselineProfile(projects.benchmarks)
+
+
+    googlePlayImplementation(platform(libs.firebase.bom))
+    googlePlayImplementation(libs.firebase.analytics)
+    googlePlayImplementation(libs.firebase.performance)
+    googlePlayImplementation(libs.firebase.crashlytics)
+
+    googlePlayImplementation(libs.firebase.cloud.messaging)
+    googlePlayImplementation(libs.firebase.remoteconfig)
+    googlePlayImplementation(libs.firebase.message)
+    googlePlayImplementation(libs.firebase.auth)
+
+    googlePlayImplementation(libs.play.game)
+    googlePlayImplementation(libs.play.update)
+    googlePlayImplementation(libs.play.update.kts)
+    googlePlayImplementation(libs.play.review)
+    googlePlayImplementation(libs.play.review.kts)
 }
 
 kotlin {
@@ -77,7 +107,8 @@ kotlin {
     androidTarget {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "1.8"            }
+                jvmTarget = "17"
+            }
         }
     }
 
@@ -89,9 +120,7 @@ kotlin {
         val jvmMain by getting
 
         androidMain.dependencies {
-            implementation(libs.kotlinx.coroutines.android)
-
-            implementation(libs.compose.ui.tooling.preview)
+            //implementation(libs.compose.ui.tooling.preview)
             implementation(libs.androidx.activity.compose)
             implementation(libs.kotlinx.coroutines.android)
 
@@ -99,38 +128,32 @@ kotlin {
         commonMain.dependencies {
 //
             implementation(libs.koin.core)
-            implementation(project(":modules:data"))
+
+            implementation(projects.modules.designsystem)
+            implementation(projects.modules.data)
+            implementation(projects.modules.ui)
+            implementation(projects.modules.model)
+            implementation(projects.modules.analytics)
+            implementation(libs.androidx.compose.material3.adaptive)
 
 
-            implementation(project(":modules:model"))
-            implementation(project(":modules:ui"))
+            implementation(projects.features.main)
+            implementation(projects.features.profile)
+            implementation(projects.features.setting)
+            implementation(projects.features.finish)
+            implementation(projects.features.question)
+            implementation(projects.features.stat)
 
 
-            implementation(project(":modules:designsystem"))
-            implementation(project(":modules:analytics"))
 
 
 
-
-            implementation(project(":features:main"))
-            implementation(project(":features:profile"))
-            implementation(project(":features:stat"))
-            implementation(project(":features:question"))
-            implementation(project(":features:finish"))
-            implementation(project(":features:setting"))
 
             // Logger
             implementation(libs.kermit)
 
             implementation(libs.kermit.koin)
 
-            implementation(libs.database)
-
-//
-//            implementation(libs.koin.compose)
-//            implementation(libs.koin.composeVM)
-//            implementation(libs.lifecycle.viewmodel.compose)
-//
 
         }
 
@@ -148,7 +171,9 @@ kotlin {
                 }
             }
         }
-
+//        configurations.commonMainApi {
+//            exclude(group = "org.jetbrains.kotlinx", module = "kotlinx-coroutines-android")
+//        }
 
     }
 }
@@ -190,7 +215,7 @@ android {
             // TODO: Abstract the signing configuration to a separate file to avoid hardcoding this.
             // signingConfig = signingConfigs.getByName("debug")
             // Ensure Baseline Profile is fresh for release builds.
-            baselineProfile.automaticGenerationDuringBuild = false
+            baselineProfile.automaticGenerationDuringBuild = true
         }
         create("benchmark") {
             // Enable all the optimizations from release build through initWith(release).
@@ -216,9 +241,6 @@ android {
         }
     }
 
-    dependencies {
-        debugImplementation(libs.compose.ui.tooling)
-    }
 }
 
 compose.desktop {
@@ -295,5 +317,7 @@ baselineProfile {
 }
 
 dependencyGuard {
-    configuration("releaseRuntimeClasspath")
+    configuration("fossReliantReleaseRuntimeClasspath")
+    configuration("googlePlayDebugRuntimeClasspath")
+
 }
