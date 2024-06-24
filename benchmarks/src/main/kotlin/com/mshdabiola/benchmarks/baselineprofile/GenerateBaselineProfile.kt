ce8081e5dcf8897cc735e8c8cbf521edc5e6cf4a
mshdabiola <mshdabiola@gmail.com>
2024-06-24 10:29:48 +0100

    feat: remove unused imports
    Removed unused imports from GenerateBaselineProfile.kt.

--- benchmarks/src/main/kotlin/com/mshdabiola/benchmarks/baselineprofile/GenerateBaselineProfile.kt
+++ benchmarks/src/main/kotlin/com/mshdabiola/benchmarks/baselineprofile/GenerateBaselineProfile.kt
@@ -6,10 +6,6 @@ package com.mshdabiola.benchmarks.baselineprofile
 
 import androidx.benchmark.macro.junit4.BaselineProfileRule
 import com.mshdabiola.benchmarks.PACKAGE_NAME
-import com.mshdabiola.benchmarks.finish.backToMain
-import com.mshdabiola.benchmarks.main.goToQuestionScreen
-import com.mshdabiola.benchmarks.question.clickOption
-import com.mshdabiola.benchmarks.question.gotoFinishScreen
 import com.mshdabiola.benchmarks.startActivity
 import org.junit.Rule
 import org.junit.Test
