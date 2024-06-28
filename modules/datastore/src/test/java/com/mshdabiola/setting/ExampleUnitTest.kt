6a0bb91ad1f037a513ba6986608dbddbc44552a6
mshdabiola <mshdabiola@gmail.com>
2024-06-28 13:49:16 +0100

    **Fix: Correct assertion in ExampleUnitTest**
    Adjusted the assertion in `ExampleUnitTest` to accurately reflect the expected size of the list.

--- modules/datastore/src/test/java/com/mshdabiola/setting/ExampleUnitTest.kt
+++ modules/datastore/src/test/java/com/mshdabiola/setting/ExampleUnitTest.kt
@@ -3,7 +3,6 @@ package com.mshdabiola.setting
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -12,7 +11,7 @@ import kotlin.test.assertEquals
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        val list= listOf(3,4,5,6)
-        assertEquals(4,list.size)
+        val list = listOf(3, 4, 5, 6)
+        assertEquals(4, list.size)
     }
 }
