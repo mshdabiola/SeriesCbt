ffaf21fc923878522ecff04122405ee1e9733b31
mshdabiola <mshdabiola@gmail.com>
2024-07-10 03:10:02 +0100

    **Refactor: Use viewModelOf for ViewModel injection**
    This commit refactors the ViewModel injection across multiple modules to use `viewModelOf` instead of `viewModel
    `. This simplifies the injection process and improves code readability.
    
    Affected modules:
    
    - finish
    - setting
    - stat
    - profile
    - question
    - main

--- features/setting/src/commonMain/kotlin/com/mshdabiola/setting/SettingModule.kt
+++ features/setting/src/commonMain/kotlin/com/mshdabiola/setting/SettingModule.kt
@@ -1,10 +1,10 @@
 package com.mshdabiola.setting
 
 import org.koin.compose.viewmodel.dsl.viewModel
+import org.koin.compose.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val settingModule = module {
-    viewModel {
-        SettingViewModel(get())
-    }
+    viewModelOf(::SettingViewModel)
+
 }
